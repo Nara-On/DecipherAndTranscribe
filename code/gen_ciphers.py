349a35f885bad64a7d375b2bb27a3ba81f22fa70
Nara _1 <marinabgbg02@gmail.com>
2024-03-15 14:07:36 +0100

    Version 2.0

--- code/gen_ciphers.py
+++ code/gen_ciphers.py
@@ -1,30 +1,31 @@
 # -*- coding: utf-8 -*-
 
 from PIL import ImageFont, ImageDraw, Image, ImageOps
+
 import random
-import shutil
 import os
 
 
 ttfs = ["Masonic_Cipher.ttf","KeilFont.ttf","Stickman.ttf", 
         "Copiale.ttf","ModernRunic-Regular.ttf", "PigpenCipher.otf"]
 
-files = ["OFL.txt"]
+files = ["big_LittleWomen.txt", "big_AlicesAdventuresInWonderland.txt", "big_Lazarillo.txt", "big_Quijote.txt"]
+lines = ["quotes.txt"]
 
 alphabet_default = "abcdefghijklmnopqrstuvwxyz "
-alphabet_copiale = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.:,!?/\\")%^&-+@~#<>{}[]=|_'
+alphabet_copiale = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.:,!?/\\")%^&-+@~#<>{}[]=|_ '
+alphabet_numerical = "0123456789 "
 
-# Gen lines
+# Gen lines rand
 min_num_chars = 20
 max_num_chars = 40
 num_lines = 10
 
 # Gen texts
-num_chars = 20
+num_chars = 25 # per image
 
 
-
-def gen_lines(ttfs):
+def gen_lines_rand(ttfs):
     
     for ttf in ttfs:
     	image_savepath = "../databases/lines/" + ttf.split(".")[0].lower() + "/images/"
@@ -34,14 +35,13 @@ def gen_lines(ttfs):
     		os.makedirs(image_savepath)
     	if not os.path.exists(groundtruth_savepath):
     		os.makedirs(groundtruth_savepath)
-            
+        
+    	border = (20, 10, 20, 10)
+    	
     	if ttf.split(".")[0].lower() == "copiale":
             selected_alphabet = alphabet_copiale
-            border = (200, 10, 200, 10)
     	else:
             selected_alphabet = alphabet_default
-            border = (20, 10, 20, 10)
-            
             
     	font = ImageFont.truetype("../ciphers/" + ttf, size=120)
         
@@ -51,25 +51,111 @@ def gen_lines(ttfs):
     	pack = []
     
     	while len(pack) <= num_lines:
-            pack.append(''.join(random.choices(selected_alphabet,weights=props)[0] for i in range(random.randint(min_num_chars,max_num_chars)))+"\n")
+            pack.append(''.join(random.choices(selected_alphabet, weights=props)[0] for i in range(random.randint(min_num_chars, max_num_chars)))+"\n")
     
-    	im = Image.new('L',(1,1),color=255)
+    	im = Image.new('L',(1,1), color=255)
     	scratch_draw = ImageDraw.Draw(im)
     	for index,text in enumerate(pack):
-            textsize = scratch_draw.multiline_textsize(text,font=font,spacing=-10)
+            textsize = scratch_draw.multiline_textsize(text, font=font, spacing=-10)
     		
-            im = Image.new('L',textsize,)
+            im = Image.new('L', textsize,)
             draw = ImageDraw.Draw(im)
-            draw.multiline_text(xy=(0,0),text=text,fill=255,font=font,spacing=-10)
+            draw.multiline_text(xy=(0,0), text=text, fill=255, font=font, spacing=-10)
             im = ImageOps.invert(im)
             im = ImageOps.expand(im, border=border, fill="white")
-            im.save(image_savepath+ttf.split(".")[0].lower()+"_"+str(index)+".png","PNG")
+            im.save(image_savepath + ttf.split(".")[0].lower() + "_" + str(index) + ".png","PNG")
             
-            f = open(groundtruth_savepath+ttf.split(".")[0].lower()+"_"+str(index)+".txt","w")
+            f = open(groundtruth_savepath + ttf.split(".")[0].lower() + "_"+str(index) + ".txt","w")
             f.write(text)
+            f.close()
+
+
+def gen_lines(ttfs, files):
+    
+    for ttf in ttfs:
+        image_savepath = "../databases/lines/" + ttf.split(".")[0].lower() + "/images/"
+        groundtruth_savepath = "../databases/lines/" + ttf.split(".")[0].lower() + "/groundtruth/"
+           
+        if not os.path.exists(image_savepath):
+       		os.makedirs(image_savepath)
+        if not os.path.exists(groundtruth_savepath):
+       		os.makedirs(groundtruth_savepath)
+           
+        border = (20, 10, 20, 10)
+       	
+        selected_alphabet = alphabet_numerical
+               
+        font = ImageFont.truetype("../ciphers/Copiale.ttf", size=120)   
                 
-
-
+        props = [5]*len(selected_alphabet)
+        props[-1] = 15
+        
+        index = 0
+        
+        for file in files:
+            l = open("../texts/" + file, "r")
+            
+            im = Image.new('L',(1,1), color=255)
+            scratch_draw = ImageDraw.Draw(im)
+            
+            for text in l.readlines():
+                   textsize = scratch_draw.multiline_textsize(text, font=font, spacing=-10)
+           		
+                   im = Image.new('L', textsize,)
+                   draw = ImageDraw.Draw(im)
+                   draw.multiline_text(xy=(0,0), text=text, fill=255, font=font, spacing=-10)
+                   im = ImageOps.invert(im)
+                   im = ImageOps.expand(im, border=border, fill="white")
+                   im.save(image_savepath + ttf.split(".")[0].lower() + "_" + str(index) + ".png","PNG")
+                   
+                   f = open(groundtruth_savepath + ttf.split(".")[0].lower() + "_" + str(index) + ".txt","w")
+                   f.write(text)
+                   f.close()
+                   index = index+1
+            l.close()
+                
+           
+def gen_lines_nums():
+    
+    image_savepath = "../databases/nums/images/"
+    groundtruth_savepath = "../databases/nums/groundtruth/"
+       
+    if not os.path.exists(image_savepath):
+   		os.makedirs(image_savepath)
+    if not os.path.exists(groundtruth_savepath):
+   		os.makedirs(groundtruth_savepath)
+       
+    border = (20, 10, 20, 10)
+   	
+    selected_alphabet = alphabet_numerical
+           
+    font = ImageFont.truetype("../ciphers/Copiale.ttf", size=120)
+       
+    props = [5]*len(selected_alphabet)
+    props[-1] = 15
+       
+    pack = []
+   
+    while len(pack) <= num_lines:
+           pack.append(''.join(random.choices(selected_alphabet, weights=props)[0] for i in range(random.randint(min_num_chars, max_num_chars)))+"\n")
+   
+    im = Image.new('L',(1,1), color=255)
+    scratch_draw = ImageDraw.Draw(im)
+    for index,text in enumerate(pack):
+           textsize = scratch_draw.multiline_textsize(text, font=font, spacing=-10)
+   		
+           im = Image.new('L', textsize,)
+           draw = ImageDraw.Draw(im)
+           draw.multiline_text(xy=(0,0), text=text, fill=255, font=font, spacing=-10)
+           im = ImageOps.invert(im)
+           im = ImageOps.expand(im, border=border, fill="white")
+           im.save(image_savepath + "copiale_" + str(index) + ".png","PNG")
+           
+           f = open(groundtruth_savepath + "copiale_"+str(index) + ".txt","w")
+           f.write(text)
+           f.close()
+            
+            
 def gen_txt(ttfs, files):
     
     for ttf in ttfs:
@@ -83,64 +169,175 @@ def gen_txt(ttfs, files):
             
     	if ttf.split(".")[0].lower() == "copiale":
                selected_alphabet = alphabet_copiale
-               border = (200, 10, 200, 10)
     	else:
                selected_alphabet = alphabet_default
-               border = (20, 10, 20, 10)
             
     	font = ImageFont.truetype("../ciphers/" + ttf, size=120)
-    	file_char = random.choice(files)
-    	file = open("../texts/" + file_char,"r",encoding = "UTF-8")
         
-    	big_text = ""
-    	for line in file:
-    		line = line.rstrip()
-    		line = line.lower()
-    		filtered = ""
-    		for c in line:
-    			if c in selected_alphabet:
-    				filtered += c
-    		big_text += filtered
+    	index = 0
+        
+    	for file_char in files:
+            file = open("../texts/" + file_char, "r", encoding="UTF-8")
+               
+            big_text = ""
+            for line in file:
+           		line = line.rstrip()
+           		line = line.lower()
+           		filtered = ""
+           		for c in line:
+           			if c in selected_alphabet:
+           				filtered += c
+           		big_text += filtered
+        
+            pack = []
+            justified_text = " "
+            countChar = 0
+            countLine = 0
+            for c in big_text:
+           		justified_text += c
+           		countChar += 1
+           		if countChar < num_chars:
+           			justified_text += " "
+           		else:
+           			countChar = 0
+           			countLine += 1
+           			if countLine < num_chars:
+           				justified_text += " \n "
+           			else:
+           				countLine = 0
+           				pack.append(justified_text)
+           				justified_text = " "
+        
+            im = Image.new('L',(1,1), color=255)
+            scratch_draw = ImageDraw.Draw(im)
+            
+            for text in pack:
+                textsize = scratch_draw.multiline_textsize(text, font=font, spacing=40)
+           		
+                im = Image.new('L',textsize,)
+                draw = ImageDraw.Draw(im)
+                draw.multiline_text(xy=(0,0), text=text,fill=255, font=font, spacing=40)
+                im = ImageOps.invert(im)
+                im.save(image_savepath + ttf.split(".")[0].lower() + "_" + str(index) + ".png","PNG")
+        
+                f = open(groundtruth_savepath + ttf.split(".")[0].lower() + "_" + str(index) + ".txt","w")
+                f.write(text)
+                f.close()
+                index = index+1
 
-    	pack = []
-    	justified_text = " "
-    	countChar = 0
-    	countLine = 0
-    	for c in big_text:
-    		justified_text += c
-    		countChar += 1
-    		if countChar < num_chars:
-    			justified_text += " "
-    		else:
-    			countChar = 0
-    			countLine += 1
-    			if countLine < num_chars:
-    				justified_text += " \n "
-    			else:
-    				countLine = 0
-    				pack.append(justified_text)
-    				justified_text = " "
 
-    	im = Image.new('L',(1,1),color=255)
-    	scratch_draw = ImageDraw.Draw(im)
-    	for index,text in enumerate(pack):
-    		textsize = scratch_draw.multiline_textsize(text,font=font,spacing=40)
-    		
-    		im = Image.new('L',textsize,)
-    		draw = ImageDraw.Draw(im)
-    		draw.multiline_text(xy=(0,0),text=text,fill=255,font=font,spacing=40)
-    		im = ImageOps.invert(im)
-    		im.save(image_savepath+ttf.split(".")[0].lower()+"_"+str(index)+".png","PNG")
 
-    		f = open(groundtruth_savepath+ttf.split(".")[0].lower()+"_"+str(index)+".txt","w")
-    		f.write(text)
 
+###########################################################################################################
+
+def gen_single_lines(ttf, text):
+    
+    savepath = "../databases/trials/lines/"
+        
+    if not os.path.exists(savepath):
+   		os.makedirs(savepath)
+       
+    border = (20, 10, 20, 10)
+   	
+    if ttf.split(".")[0].lower() == "copiale":
+           selected_alphabet = alphabet_copiale
+    else:
+           selected_alphabet = alphabet_default
+           
+    font = ImageFont.truetype("../ciphers/" + ttf, size=120)
+       
+    props = [5]*len(selected_alphabet)
+    props[-1] = 15
+   
+    im = Image.new('L',(1,1), color=255)
+    scratch_draw = ImageDraw.Draw(im)
+    
+    textsize = scratch_draw.multiline_textsize(text, font=font, spacing=-10)
+    
+    im = Image.new('L', textsize,)
+    
+    draw = ImageDraw.Draw(im)
+    draw.multiline_text(xy=(0,0), text=text, fill=255, font=font, spacing=-10)
+    im = ImageOps.invert(im)
+    im = ImageOps.expand(im, border=border, fill="white")
+    im.save(savepath + ttf.split(".")[0].lower() + ".png","PNG")
+           
+    f = open(savepath + ttf.split(".")[0].lower() + ".txt","w")
+    f.write(text)
+    f.close()
+        
+
+def gen_single_txt(ttf, file):
+    
+    image_savepath = "../databases/trials/texts/"
+       
+    if not os.path.exists(image_savepath):
+   		os.makedirs(image_savepath)
+           
+    if ttf.split(".")[0].lower() == "copiale":
+        selected_alphabet = alphabet_copiale
+    else:
+        selected_alphabet = alphabet_default
+           
+    font = ImageFont.truetype("../ciphers/" + ttf, size=120)
+    file = open("../texts/" + file, "r", encoding="UTF-8")
+       
+    big_text = ""
+    for line in file:
+   		line = line.rstrip()
+   		line = line.lower()
+   		filtered = ""
+   		for c in line:
+   			if c in selected_alphabet:
+   				filtered += c
+   		big_text += filtered
+
+    pack = []
+    justified_text = " "
+    countChar = 0
+    countLine = 0
+    for c in big_text:
+   		justified_text += c
+   		countChar += 1
+   		if countChar < num_chars:
+   			justified_text += " "
+   		else:
+   			countChar = 0
+   			countLine += 1
+   			if countLine < num_chars:
+   				justified_text += " \n "
+   			else:
+   				countLine = 0
+   				pack.append(justified_text)
+   				justified_text = " "
+
+    im = Image.new('L',(1,1), color=255)
+    scratch_draw = ImageDraw.Draw(im)
+    for index,text in enumerate(pack):
+   		textsize = scratch_draw.multiline_textsize(text, font=font, spacing=40)
+   		
+   		im = Image.new('L',textsize,)
+   		draw = ImageDraw.Draw(im)
+   		draw.multiline_text(xy=(0,0), text=text,fill=255, font=font, spacing=40)
+   		im = ImageOps.invert(im)
+   		im.save(image_savepath + ttf.split(".")[0].lower() + "_"+str(index) + ".png","PNG")
+
+   		f = open(image_savepath + ttf.split(".")[0].lower() + "_"+str(index) + ".txt","w")
+   		f.write(text)
+   		f.close()
+                
+###########################################################################################################
 
 
 if __name__ == '__main__':
-	#gen_lines(ttfs)
+    
+	#gen_lines(ttfs, lines)
+    #gen_lines_rand(ttfs)
+    #gen_lines_nums()
     #gen_txt(ttfs, files)
     
-    pass
-    
-    
+    """
+    for ttf in ttfs:
+        gen_single_lines(ttf, "hello world")
+        gen_single_txt(ttf, "mid_Ozymandias.txt")
+    """    
